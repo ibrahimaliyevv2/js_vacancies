6e348c2e9c7b038888832e4f782698602b8b99e3
Ulvi Memmedov <onezerolord@gmail.com>
2022-09-08 21:55:36 +0400

    ds

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 //Show to admin
 
 var vacancies = [];
+var categoryies = [];
 var errors = {};
+var currentUser = {};
+var buttonDsiableList = [];
 
 const addForm = document.querySelector('#add_form');
 const searchInput = document.querySelector('#search-input');
@@ -34,27 +37,53 @@ addForm.addEventListener("submit", (e) => {
 
 //vacansies
 class Vacancy {
-    constructor({ id, name, category }) {
+    constructor({ id, name, category, admin_id }) {
         this.id = id;
         this.name = name;
         this.category = category;
+        this.admin_id = admin_id;
+        this.applied = [];
     }
+
     save() {
         vacancies.push(this);
         return this.get();
     }
+
     get() {
         return {
             id: this.id,
             name: this.name,
-            category: this.category
+            category: this.category,
+            admin_id: this.admin_id
+        };
+    }
+}
+
+class Category {
+    constructor({ id, name }) {
+        this.id = id;
+        this.name = name;
+    }
+
+    save() {
+        categoryies.push(this);
+        return this.get();
+    }
+
+    get() {
+        return {
+            id: this.id,
+            name: this.name
         };
     }
 }
 
 //CRUD operations
 function addVacancy(credentials) {
-
+    const cat = new Category({ id: categoryies[categoryies.length - 1] ? (categoryies[categoryies.length - 1].id + 1) : 1, name: credentials.category });
+    cat.save();
+    credentials['category'] = cat.id;
     return (new Vacancy(credentials)).save();
 }
 
@@ -101,6 +130,7 @@ function getFormData(e, vacancy_id) {
         let value = child.lastElementChild.value;
         obj[id] = value;
         obj["id"] = vacancy_id ? vacancy_id : vacancies[vacancies.length - 1] ? (vacancies[vacancies.length - 1].id + 1) : 1;
+        obj["admin_id"] = currentUser.id;
     });
 
     return obj;
@@ -172,6 +202,14 @@ function fillForm(selected, vacancy_id, vacancy) {
 
     });
 }
+// helpers
+function apply(e) {
+    let id = Number(e.id.split("_").pop());
+    buttonDsiableList.push({ user_id: currentUser.id, vacancy_id: id });
+    let index = vacancies.findIndex(e => e.id == id);
+    vacancies[index].applied.push(currentUser.id);
+    generateUI();
+}
 
 //generation of UI
 function generateUI(searched = []) {
@@ -186,39 +224,41 @@ function generateUI(searched = []) {
     tableBodyForClient.innerHTML = "";
     tableHeadForClient.innerHTML = "";
 
+
     let list = searched.length ? searched : vacancies;
 
     if (list.length) {
         Object.keys(vacancies[0]).forEach(key => {
-            const th = document.createElement("th");
+            if (key != "admin_id") {
+                const th = document.createElement("th");
+                th.textContent = key;
+                tableHead.append(th);
+            }
 
-            th.textContent = key;
 
-            tableHead.append(th);
-            tableHeadForClient.append(th);
         });
-        list.forEach(vacancy => {
-            if (vacancy && Object.keys(vacancy).length) {
-                const trBody = document.createElement("tr");
+        if (currentUser.isAdmin) {
+            list.filter(d => d.admin_id == currentUser.id).forEach(vacancy => {
+                if (vacancy && Object.keys(vacancy).length) {
+                    const trBody = document.createElement("tr");
 
-                trBody.addEventListener("click", getRowData);
+                    trBody.addEventListener("click", getRowData);
 
-                trBody.innerHTML = `
+                    trBody.innerHTML = `
                     <td scope="row">${vacancy.id}</td>
                     <td>${vacancy.name}</td>
-                    <td>${vacancy.category}</td>
+                    <td>${categoryies.find(e => e.id == vacancy.category).name}</td>
+                    <td>${vacancy.applied.length}</td>
                     <td>
                         <button id="edit_${vacancy.id}" onclick="getRowData(this)" type="button" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></button>
                         <button id="delete_${vacancy.id}" onclick="deleteVacancy(this)" type="button" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
                     </td>
                 `;
-                tableBody.append(trBody);
-                // tableBodyForClient.append(trBody);
-            }
-        });
-
-        list.forEach(vacancy => {
-            if (vacancy && Object.keys(vacancy).length) {
+                    tableBody.append(trBody);
+                }
+            })
+        } else {
+            list.forEach(vacancy => {
                 const trBody = document.createElement("tr");
 
                 trBody.addEventListener("click", getRowData);
@@ -227,11 +267,13 @@ function generateUI(searched = []) {
                     <td scope="row">${vacancy.id}</td>
                     <td>${vacancy.name}</td>
                     <td>${vacancy.category}</td>
+                    <td><button ${buttonDsiableList.find(d => d.vacancy_id == vacancy.id && d.user_id == currentUser.id) ? "disabled" : null} id="apply_${vacancy.id}" class="btn" onclick="apply(this)">muraciet et</button></td>
                 `;
-                // tableBody.append(trBody);
                 tableBodyForClient.append(trBody);
-            }
-        });
+            });
+        }
+
+
     }
     else {
         tableBody.innerHTML = "Data not found"
@@ -280,7 +322,8 @@ const users = [
         id: 3,
         login: 'client1',
         password: '12345',
-        isAdmin: false
+        isAdmin: false,
+
     },
     {
         id: 4,
@@ -297,8 +340,8 @@ document.querySelector("#loginForm").addEventListener("submit", function (e) {
 });
 
 function login() {
-    const loginTry = document.getElementById("username").value;
-    const passTry = document.getElementById("password").value;
+    let loginTry = document.getElementById("username").value;
+    let passTry = document.getElementById("password").value;
     const checkedUser = users.find(user => {
         return user.login === loginTry && user.password === passTry
     })
@@ -308,11 +351,15 @@ function login() {
         loginPageContainer.style.display = "none";
         toClientContainer.style.display = "none";
         toAdminContainer.style.display = "block";
+        currentUser = checkedUser;
+        generateUI();
     }
     else {
         loginPageContainer.style.display = "none";
         toAdminContainer.style.display = "none";
         toClientContainer.style.display = "block";
+        currentUser = checkedUser;
+        generateUI();
     }
 
     loginTry = "";
@@ -325,7 +372,7 @@ function login() {
 
 const logOutButton = document.querySelectorAll(".logout");
 
-logOutButton.forEach(btn=>{
+logOutButton.forEach(btn => {
     btn.addEventListener("click", (e) => {
         e.preventDefault();
         logOut();
@@ -347,25 +394,3 @@ function logOut() {
 
 const searchInputForClient = document.querySelector("#search-input_client");
 
-//Many to many relation
-
-var userVacancies = [];
-
-class UserVacancy {
-    constructor({ id }) {
-        this.id = id;
-        this.userId = users.find(user =>user.login === document.getElementById("username").value && user.password === document.getElementById("password").value);
-        this.vacancyId = vacancyId;
-    }
-    save() {
-        userVacancies.push(this);
-        return this.get();
-    }
-    get() {
-        return {
-            id: this.id,
-            userId: this.userId,
-            vacancyId: this.vacancyId
-        };
-    }
-}
